34cb59e72deaff0f7cda233580fade7ff6567a56
isalunbel <142603867+isalunbel@users.noreply.github.com>
2024-04-05 18:33:11 +0300

    Update Application.java

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,26 +1,12 @@
 package org.example;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.example.repository.UserRepository;
-import ru.netology.repository.UserRepository;
-import ru.netology.service.AuthorizationService;
-import org.example.service.AuthorizationService;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    private UserRepository userRepository() {
-        return new UserRepository();
-    }
-
-    @Bean
-    private AuthorizationService authorizationService(final UserRepository userRepository) {
-        return new AuthorizationService(userRepository);
     }
 }
